931a1e1fbf2187bc0945126a1d9bc9cc96f30610
Pooja-Gupta07 <120452820+Pooja-Gupta07@users.noreply.github.com>
2023-03-19 21:35:09 +0530

    Add files via upload

---
+++ tbp_script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["An Inspiration", "Helper", "Feeds People", "Theosophist","Nobel price for peace", "Poor People's God", "Reformer",],
+    typeSpeed:140,
+    backSpeed:150,
+    loop:true
+});
